18c49bad8e85b13f345f1b57fe04bb9f63e630f8
Mjcapunitan <mjbcapunitan@gmail.com>
2025-01-06 09:44:55 +0800

    anghulinghalakhak

--- src/components/pages/backend/access/DeveloperProtectedRoute.jsx
+++ src/components/pages/backend/access/DeveloperProtectedRoute.jsx
@@ -62,10 +62,12 @@ const DeveloperProtectedRoute = ({ children }) => {
           <FetchingSpinner />
         ) : isAuth === "123" ? (
           children
-        ) : (
+        ) : isAuth === "456" ? (
           <Navigate to={`${devNavUrl}/developer/login`} />
+        ) : (
+          <p>End point Not Found.</p>
         )}
-        ;
+        
       </>
     );
   }
